a213f908781dd071b596fa23eeb0589173c51b93
ESOLUTIONS\Rashmi.Singh <rashmi.singh@trivium-esolutions.com>
2023-05-25 16:31:34 +0530

    clean-up redundant imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Footer() {
   return <p>This is a practice app</p>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Header() {
   return <h1>Welcome to the page!</h1>;
 }
